b5a9653ff314120c64aa87f63c5a95549aa8d709
Lucccyo <cha.git@mailo.fr>
2022-11-03 00:41:13 +0100

    j'ai envie de finir :c

--- randomized_prim/maze.ml
+++ randomized_prim/maze.ml
@@ -1,6 +1,9 @@
 type cell = {we: bool; ws: bool}
 type wall = {x:int; y: int; e_or_s: bool}
 
+let l = 4
+let h = 3
+
 (* val add : wall array * int -> wall -> wall array * int *)
 let add (w_arr : wall array * int) wall =
   let l     = snd w_arr in
@@ -15,21 +18,24 @@ let remove (w_arr : wall array * int) r =
   w_arr.(r) <- w_arr.(l - 1);
   (w_arr, l - 1)  
 
-val is_not_visited grid c i = 
+(* val is_not_visited : cell array -> cell -> int -> bool *)
+let is_not_visited grid c i = 
   let n = i - l in
   let w = i - 1 in
-  let wn = try (grid.(n)).ws with true in
-  let ww = try (grid.(w)).we with true in
+  let wn = try (grid.(n)).ws with Invalid_argument _e -> true in
+  let ww = try (grid.(w)).we with Invalid_argument _e -> true in
   c.we && c.ws && wn && ww
 
-(* val break : cell array -> int -> int -> bool -> int *)
-let break grid x y e l =
+let break grid x y e r =
   let i = y * l + x in
   let c_i = grid.(i) in
-  let neighbour = if e then i + 1 else i + l in
-  let c_neighbour = grid.(neighbour) in
-  is_not_visited grid c_i i lor is_not_visited grid c_neighbour neighbour
+  let j = if e then i + 1 else i + l in
+  let c_j = grid.(j) in
+  let breakable = is_not_visited grid c_i i lor is_not_visited grid c_j j in
+  if breakable
+  then 
+  else 
 
 
-  
+
 
