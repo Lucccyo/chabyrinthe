0bd1d005f7953d843ae18662f24184e02d1fbebf
Lucccyo <cha.git@mailo.fr>
2022-11-05 20:31:59 +0100

    fix

--- randomized_prim/maze.ml
+++ randomized_prim/maze.ml
@@ -2,8 +2,13 @@ type cell = {we: bool; ws: bool}
 type wall = {x:int; y: int; e_or_s: bool}
 type dyn_w = (wall array * int)
 
-let l = 4
-let h = 4
+let l = 2
+let h = 2
+
+let display_wall_array a l = 
+  for i = 0 to l - 1 do
+    Format.printf "x = %d\ty = %d\t%s\n%!" a.(i).x a.(i).y (if a.(i).e_or_s then "east" else "south")
+  done
 
 (* val add : dyn_w -> wall -> dyn_w *)
 let add (w_arr : wall array * int) wall =
@@ -29,16 +34,18 @@ let is_not_visited grid c i =
 
 (* val break : wall -> cell array -> int -> dyn_w -> dyn_w *)
 let break w grid r w_arr =
+  Format.printf "\n\nw_arr status :\n";
+  display_wall_array (fst w_arr) (snd w_arr);
   let i = w.y * l + w.x in
-  Format.printf "x = %d; y = %d; l = %d; i = %d\n"w.x w.y l i;
+  Format.printf "chose wall: x = %d; y = %d; east? = %b; l = %d; i = %d\n"w.x w.y w.e_or_s l i;
   let c_i = grid.(i) in
-  Format.printf "bou2\n";
   let i_not_visited = is_not_visited grid c_i i in
   let j = if w.e_or_s then i + 1 else i + l in
   let j_not_visited =
     (try 
       is_not_visited grid (grid.(j)) j 
     with Invalid_argument _ -> false) in
+    Format.printf "i notvisited? : %b\tj notvisited? : %b\n%!" i_not_visited j_not_visited;
   let breakable = i_not_visited || j_not_visited in
   if not breakable
   then remove w_arr r
@@ -70,7 +77,9 @@ let break w grid r w_arr =
           {x = w.x + 1; y = w.y + 1; e_or_s = false};
           {x = w.x - 1; y = w.y + 1; e_or_s = true}] in
       let l = li @ lj in
+      Format.printf "bou1\n";
       let updated = List.fold_left (fun acc e -> add acc e) w_arr l in
+      Format.printf "bou2\n";
       remove updated r
       )
 
@@ -84,14 +93,14 @@ let rec progress (w_arr : wall array * int) grid =
     progress next_w_arr grid 
 
 let rec init_walls i walls col line = 
-  Format.printf "i == %d\n" i;
+  (* Format.printf "i == %d\n" i; *)
   if i = Array.length walls
   then walls
   else (
     Format.printf "x = %d\ty = %d\teast = %b\n" (col) (line) (true);
-    walls.(i)   <- {x = i mod l; y = i / l; e_or_s = true};
+    walls.(i)   <- {x = col; y = line; e_or_s = true};
     Format.printf "x = %d\ty = %d\teast = %b\n" (col) (line) (false);
-    walls.(i+1) <- {x = i mod l; y = i / l; e_or_s = false};
+    walls.(i+1) <- {x = col; y = line; e_or_s = false};
     let new_col  = (col+1) mod l in
     let new_line = (if col = new_col - 1 then line else line + 1) in
     init_walls (i+2) walls new_col new_line)
@@ -100,15 +109,6 @@ let init l h =
   let grid  = Array.make (l * h) {we = true; ws =  true} in
   let walls = Array.make (l * h * 2) {x = 0; y = 0; e_or_s = true} in
   let walls = init_walls 0 walls 0 0 in
-  (* for i = 0 to ((l * h * 2) - 1) do
-    for x = 0 to (l-1) do
-      for y = 0 to (h-1) do
-
-      done
-    done
-    Format.printf "x = %d\ty = %d\teast = %b\n" (i mod l) (i / l) (i mod 2 = 0);
-    walls.(i) <- {x = (i mod l); y = (i / l); e_or_s = i mod 2 = 0}
-  done; *)
   let init_arr = (Array.make (l * h * 2) {x = 0; y = 0; e_or_s = true}, 0) in
   let r = Random.int (l * h * 2) in
   (* Format.printf "r = %d\t\tinit_arr = %d\t\tl_init_arr = %d\t\twalls = %d\n%!" r (Array.length (fst init_arr)) (snd init_arr) (Array.length walls); *)
