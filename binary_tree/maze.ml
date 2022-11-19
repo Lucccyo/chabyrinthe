9d2d28213b21f28cc1547d23517d16536ede5615
Lucccyo <cha.git@mailo.fr>
2022-11-19 09:09:14 +0100

    remove trailing spaces

--- binary_tree/maze.ml
+++ binary_tree/maze.ml
@@ -6,12 +6,12 @@ type cell = {
 let l = int_of_string Sys.argv.(1)
 let h = int_of_string Sys.argv.(2)
 
-let print_cell c = 
+let print_cell c =
   if c.south
   then Format.printf "___"
   else Format.printf "  |"
 
-let rec print_grid grid c l h = 
+let rec print_grid grid c l h =
   if c >= (h * l)
   then ()
   else (let line = Array.sub grid c l in
@@ -19,12 +19,12 @@ let rec print_grid grid c l h =
     Format.printf "\n";
     print_grid grid (c+l) l h)
 
-let display grid = print_grid grid 0 l h 
+let display grid = print_grid grid 0 l h
 
 let grid = Array.make (l * h) ({south = true ; east = true})
 
-let maze = Array.map (fun c -> 
+let maze = Array.map (fun c ->
   Random.self_init ();
-  if (Random.int 100) mod 2 = 0 
-  then {south = false   ; east = c.east} 
+  if (Random.int 100) mod 2 = 0
+  then {south = false   ; east = c.east}
   else {south = c.south ; east = false}) grid |> display
